73a18f257863ae587ac1256bcd93539425121724
eunji woo <eunji20392@naver.com>
2020-08-15 17:30:24 +0900

    guru

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-"""my_site_prj URL Configuration
 
+"""my_site_prj URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,16 @@ Including another URLconf
 from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    path('', views.PostList.as_view()),
 
+urlpatterns = [
+    path('search/<str:q>/', views.PostSearch.as_view()),
+    path('tag/<str:slug>/', views.PostListByTag.as_view()),
+    path('category/<str:slug>/', views.PostListByCategory.as_view()),
+    path('edit_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/', views.delete_comment),
+    path('<int:pk>/new_comment/', views.new_comment),
+    path('<int:pk>/update/', views.PostUpdate.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('create/', views.PostCreate.as_view()),
+    path('', views.PostList.as_view()),
 ]
